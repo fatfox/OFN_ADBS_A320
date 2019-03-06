6fa40aba041f98b00ed253844e12c29ed98381b5
fatfox <wander_temp@126.com>
2019-03-06 13:35:39 +0800

    Update ADBS_A320.ino

--- ADBS_A320.ino
+++ ADBS_A320.ino
@@ -69,42 +69,44 @@ void loop()
 void Init_A320(void)
 { 
   //--------------Power Up----------------------
+  // Datasheet: Notes on Power-up
+  // 1. Apply power
+  // 2.
   digitalWrite(SHTDWNPIN, LOW);
+  // 模块方向已经固定
 //  digitalWrite(OrientPIN, LOW);
   //delay(100);
+  // 3. TWI 地址已经固定
+  // 4. NRST toggle
   digitalWrite(NRSTPIN, LOW);
   delayMicroseconds(2);
   digitalWrite(NRSTPIN, HIGH);
   //delay(10);
 //  digitalWrite(OrientPIN, HIGH);
-  // Serial.println("Hello A320-1");
   
   //------------Enable I2C------------
-  Wire.begin();// join i2c bus (address optional for master)
+  Wire.begin();
   delay(100);
   Wire.beginTransmission(ADDR);
   Wire.endTransmission();
   delayMicroseconds(2);
   
+  // Init.
+  // 5. TWI 不读取 Product ID 也行
   read_register(ADDR, Product_ID);
-  Serial.print("Product_ID: ");
-  Serial.println(Val, HEX);
-  
-  set_register(ADDR, Soft_RESET, 0x56);
+  // 6. TWI 不需要软重置
+/*  set_register(ADDR, Soft_RESET, 0x56);
   delayMicroseconds(10);
-  read_register(ADDR, Soft_RESET);
-  Serial.print("Soft_RESET: ");
-  Serial.println(Val, HEX);
+  read_register(ADDR, Soft_RESET);*/
   
+  // 7.
   set_register(ADDR, OFN_Engine, 0xe4); 
-  //set_register(ADDR, OFN_Engine, 0xf6); 
+  // 8.
   set_register(ADDR, OFN_Resolution, 0x12);   //500cpi
-  read_register(ADDR, OFN_Resolution);
-  Serial.print("OFN_Resolution: ");
-  Serial.println(Val, HEX);
- 
+//  read_register(ADDR, OFN_Resolution);
   set_register(ADDR, OFN_Speed_Control, 0x0e);//Speed of switching 12ms, low cpi 250
-  //set_register(ADDR, OFN_Speed_Control, 0xce);
+  
+  // 9. Check 是什么意思？(set or read?)
   set_register(ADDR, OFN_Speed_ST12, 0x08);
   set_register(ADDR, OFN_Speed_ST21, 0x06);
   set_register(ADDR, OFN_Speed_ST23, 0x40);
@@ -113,18 +115,31 @@ void Init_A320(void)
   set_register(ADDR, OFN_Speed_ST43, 0x0a);
   set_register(ADDR, OFN_Speed_ST45, 0x50);   //500cpi
   set_register(ADDR, OFN_Speed_ST54, 0x48);   //Speed of switching 12ms, low cpi 250
-  //set_register(ADDR, OFN_AD_CTRL, 0xc4);
+  
+  
+  // 10. Check...
+  set_register(ADDR, OFN_AD_CTRL, 0xc4); // ?
   set_register(ADDR, OFN_AD_ATH_HIGH, 0x34);
   set_register(ADDR, OFN_AD_DTH_HIGH, 0x3c);  //500cpi
   set_register(ADDR, OFN_AD_ATH_LOW, 0x18);
   set_register(ADDR, OFN_AD_DTH_LOW, 0x20);
+  
+  // 11.
   set_register(ADDR, OFN_FPD_CTRL, 0x50);
+  
+  // 12.
   set_register(ADDR, OFN_Quantize_CTRL, 0x99);
   set_register(ADDR, OFN_XYQ_THRESH, 0x02);
   
+  // 13. burst mode if used
+  
+  // 14. 
   read_register(ADDR, Motion);
   read_register(ADDR, Delta_X);
   read_register(ADDR, Delta_Y);
+  
+  // 15. LED_Control
+  // ...
 
   Serial.println("Ready.");
 }
